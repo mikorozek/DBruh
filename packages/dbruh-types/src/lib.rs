5dd1d861f1c9e6abd50746675cbffd0d6f6871a0
mikorozek <mikolajrozek0@gmail.com>
2025-07-20 18:42:51 +0200

    Implemented Row Operation type and all necessary things for writing data

--- packages/dbruh-types/src/lib.rs
+++ packages/dbruh-types/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -5,18 +7,65 @@ pub enum DataType {
     Text,
     Integer,
     Boolean,
-    Uuid,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct ColumnDefinition {
-    pub name: String,
     pub data_type: DataType,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct TableSchema {
     pub table_name: String,
-    pub columns: Vec<ColumnDefinition>,
-    pub primary_key_column: String,
+    pub columns: HashMap<String, ColumnDefinition>,
+    pub primary_key_columns: Vec<String>,
+}
+
+#[derive(Clone, PartialEq)]
+pub enum DataValue {
+    Text(String),
+    Integer(i32),
+    Boolean(bool),
+    Null,
+}
+
+#[derive(Clone)]
+pub struct UpdateValue {
+    pub column_name: String,
+    pub data_value: DataValue,
+}
+
+#[derive(Clone)]
+pub enum RowOperationType {
+    RowOperation(UpdateValue),
+    Delete,
+}
+
+#[derive(Clone)]
+pub struct RowOperation {
+    pub op_type: RowOperationType,
+    pub timestamp: u64,
+}
+
+impl PartialEq for RowOperation {
+    fn eq(&self, other: &Self) -> bool {
+        self.timestamp == other.timestamp
+    }
+    fn ne(&self, other: &Self) -> bool {
+        self.timestamp != other.timestamp
+    }
+}
+
+impl Eq for RowOperation {}
+
+impl PartialOrd for RowOperation {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.timestamp.partial_cmp(&other.timestamp)
+    }
+}
+
+impl Ord for RowOperation {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.timestamp.cmp(&other.timestamp)
+    }
 }
